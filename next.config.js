d58af1998ce8bfb379fae311e953fd7271723947
soowan <nick6767676767@gmail.com>
2025-08-20 23:40:06 +0900

    fix: cors 설정 추가

--- next.config.js
+++ next.config.js
@@ -12,6 +12,27 @@ const nextConfig = {
         domains: ["localhost", "ulsantour.vercel.app"],
         unoptimized: true,
     },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://ulsantour.vercel.app",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-Requested-With, Content-Type",
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 export default nextConfig
