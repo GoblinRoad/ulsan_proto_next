adbde4f94e9945a589edef13c355609199837d7c
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-08-29 01:10:45 +0900

    chore: cors 변경

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        domains: ["localhost", "ulsan-proto-next.vercel.app"],
-        unoptimized: true,
-    },
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "https://ulsan-proto-next.vercel.app",
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,POST,OPTIONS",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-Requested-With, Content-Type",
-                    },
-                ],
-            },
-        ]
-    },
-}
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["localhost", "ulsan-proto-next.vercel.app"],
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://ulsan-proto-next.vercel.app,https://ulsantour.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
